19001a5b58aab5a6a5ae4366e76271d8aa40e4a1
chintan <chintanjoshi2012>
2018-12-02 16:06:26 -0800

    Final commit to weather app

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,13 +1,9 @@
 import React from "react";
-class Titles extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
-      </div>
-    );
-  }
-}
+const Titles = (props) => (
+  <div>
+    <h1 className = "titl-container__title">Weather Finder</h1>
+    <h5 className="title-container__subtitle">Find out temperature, conditions and more...</h5>
+  </div>
+);
 
 export default Titles;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,27 +1,42 @@
 import React from "react";
-class Weather extends React.Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        {this.props.city && this.props.country && (
-          <p>
-            Location:{this.props.city},{this.props.country}
-          </p>
-        )}
-        {this.props.city && this.props.country && (
-          <p>Temperature:{this.props.temperature}</p>
-        )}
-        {this.props.city && this.props.country && (
-          <p> Humidity:{this.props.humidity}</p>
-        )}
-        {this.props.city && this.props.country && (
-          <p>Condition: {this.props.description}</p>
-        )}
-        {this.props.error && <p>{this.props.error}</p>}
-      </div>
-    );
-  }
-}
+
+const Weather = props => (
+    <div>
+      {
+        props.city && props.country && (
+        <p className= "weather__key">
+          Location:
+          <span className = "weather__value"> {props.city},{props.country}</span>
+        </p>
+      )}
+      {
+        props.city && props.country && (
+        <p className= "weather__key">
+          Temperature:
+          <span className = "weather__value"> {props.temperature}</span>
+        </p>
+      )}
+      {
+        props.city && props.country && (
+        <p className= "weather__key">
+          Humidity:
+          <span className = "weather__value"> {props.humidity}</span>
+        </p>
+      )}
+      {
+        props.city && props.country && (
+        <p className= "weather__key">
+          Condition:
+          <span className = "weather__value"> {props.description}</span>
+        </p>
+      )}
+      {
+        props.error && (
+        <p className= "weather__error">
+          {props.error}
+      </p>
+    )}
+    </div>
+);
 
 export default Weather;
